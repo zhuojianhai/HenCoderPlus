52f321b92aed92db1f3195d4b9962851c4fac1d0
zhuojianhai@126.com <zhuojianhai@126.com>
2020-10-27 14:34:58 +0800

    嵌套view

---
+++ 15_drag_nestedscroll/src/main/java/com/hencoder/a15_drag_nestedscroll/DragUpDownLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.hencoder.a15_drag_nestedscroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DragUpDownLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag_up_down_layout);
+    }
+}
--- 15_drag_nestedscroll/src/main/java/com/hencoder/a15_drag_nestedscroll/MainActivity.java
+++ 15_drag_nestedscroll/src/main/java/com/hencoder/a15_drag_nestedscroll/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hencoder.a15_drag_nestedscroll;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.constraintlayout.solver.widgets.ConstraintWidget;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,14 +16,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button bt_draghelper_gridview;
     Button bt_DragListenerGridView;
+    Button bt_DragToCollectLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nested_scalable_image_view);
         bt_draghelper_gridview = findViewById(R.id.bt_draghelper_gridview);
         bt_DragListenerGridView = findViewById(R.id.bt_DragListenerGridView);
+        bt_DragToCollectLayout = findViewById(R.id.bt_DragToCollectLayout);
         bt_draghelper_gridview.setOnClickListener(this);
         bt_DragListenerGridView.setOnClickListener(this);
+        bt_DragToCollectLayout.setOnClickListener(this);
 
     }
 
@@ -32,6 +38,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }else if (v == bt_DragListenerGridView){
             Intent intent = new Intent(this,DragListenerGridViewActivity.class);
             startActivity(intent);
+        }else if (v == bt_DragToCollectLayout){
+            Intent intent = new Intent(this,DragToCollectLayoutActivity.class);
+            startActivity(intent);
         }
+
+        RecyclerView recycleView = new RecyclerView(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
     }
 }
