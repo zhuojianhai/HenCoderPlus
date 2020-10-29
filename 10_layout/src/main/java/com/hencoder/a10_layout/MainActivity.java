e9e630f17a701122b2dc1969f9af78ceba530496
zhuojianhai@126.com <zhuojianhai@126.com>
2020-10-29 13:54:16 +0800

    自定义view学习

--- 10_layout/src/main/java/com/hencoder/a10_layout/MainActivity.java
+++ 10_layout/src/main/java/com/hencoder/a10_layout/MainActivity.java
@@ -1,7 +1,14 @@
 package com.hencoder.a10_layout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams  params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setLayoutParams(params);
+
+
+
+        RecyclerView recyclerView = new RecyclerView(this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        RecyclerView.Adapter<MyAdapterViewHolder> adapterAdapter = new RecyclerView.Adapter<MyAdapterViewHolder>() {
+            @NonNull
+            @Override
+            public MyAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyAdapterViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        };
+        recyclerView.setAdapter(adapterAdapter);
+
+        linearLayout.addView(recyclerView);
+
+    }
+
+    private class MyAdapterViewHolder extends RecyclerView.ViewHolder {
+        public MyAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
--- 15_drag_nestedscroll/src/main/java/com/hencoder/a15_drag_nestedscroll/MainActivity.java
+++ 15_drag_nestedscroll/src/main/java/com/hencoder/a15_drag_nestedscroll/MainActivity.java
@@ -1,14 +1,17 @@
 package com.hencoder.a15_drag_nestedscroll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.constraintlayout.solver.widgets.ConstraintWidget;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ScrollView;
 
@@ -46,5 +49,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         RecyclerView recycleView = new RecyclerView(this);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
+        RecyclerView.Adapter<MyViewHolder> adapter = new RecyclerView.Adapter<MyViewHolder>() {
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        };
+
+        recycleView.setAdapter(adapter);
+
+        /***
+         * 如何判断RecyclerView是否滚动到底部或顶部?
+         *
+         * 以更常见的纵向来说，使用Recyclerview. canScrollVertically(1)，
+         * 当返回值是false的时候，代表你的RecyclerView不能继续往下滚动啦，也就是说已经滚动到底部了。
+         *
+         * 同理，当Recyclerview. canScrollVertically(-1)返回false的时候代表RecyclerView不能继续网上滚动了，已经到顶部了。
+         * 这个方法比通过计算当前item的位置以及总item数量的方法进行判断要好多了。
+         */
+        boolean canscroll = recycleView.canScrollVertically(1);
+
     }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    ItemTouchHelper.Callback itemTouchHelperCallback = new ItemTouchHelper.Callback() {
+        @Override
+        public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+            return 0;
+        }
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+    };
+
 }
